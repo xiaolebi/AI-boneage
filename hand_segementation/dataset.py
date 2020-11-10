d7ba153f8a659e040338a6c44f292cfb856264f5
xiaolebi <73866848+xiaolebi@users.noreply.github.com>
2020-11-10 11:06:34 +0800

    Update dataset.py

--- hand_segementation/dataset.py
+++ hand_segementation/dataset.py
@@ -42,21 +42,21 @@ class HandDataSet(DATA.Dataset):
                     img = img.transpose(Image.ROTATE_270)
                     mask = mask.transpose(Image.ROTATE_270)
                 mask = np.array(mask).astype(np.uint8)
-                mask = cv2.resize(mask,(512,512))
+#                 mask = cv2.resize(mask,(512,512))
                 mask = np.array(mask).astype(np.int64)
                 img = np.array(img.convert('RGB')).astype(np.float32)
                 img = img/255.0
-                if np.random.random()<0.5:
-                    img = np.fliplr(img)
-                    mask = np.fliplr(mask)
+#                 if np.random.random()<0.5:
+#                     img = np.fliplr(img)
+#                     mask = np.fliplr(mask)
                 mask = mask[...,np.newaxis]
-                img = cv2.resize(img,(512,512))
+#                 img = cv2.resize(img,(512,512))
             return np.transpose(img,[2,0,1]).copy(),mask.copy()
         else:
             img_path = os.path.join(self.train_path,self.image_list[index])
             img = np.array(Image.open(img_path).convert('RGB')).astype(np.float32)
             img /= 255.0
-            img = np.resize(img, (512, 512))
+#             img = np.resize(img, (512, 512))
             return np.transpose(img,[2,0,1]).copy(),self.image_list[index]
     def __len__(self):
         return  len(self.image_list)
