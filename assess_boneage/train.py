4b7cbc5736ba0794d9ac57304a0128c7698c2320
xiaolebi <73866848+xiaolebi@users.noreply.github.com>
2020-12-30 18:15:29 +0800

    Update train.py

--- assess_boneage/train.py
+++ assess_boneage/train.py
@@ -24,7 +24,7 @@ parser.add_argument('--epochs',default=100,type=int,metavar='N',help='number of 
 parser.add_argument('--start_epoch',default=0,type=int,metavar='N',help='manual epoch number (useful on restarts)')
 parser.add_argument('--train_batch',default=16,type=int,metavar='N',help='train batch size')
 parser.add_argument('--test_batch',default=3,type=int,metavar='N',help='test batch size')
-parser.add_argument('--lr','--learning-rate',default=0.001,type=float,metavar='LR',help='initial learning rate')
+parser.add_argument('--lr','--learning-rate',default=0.000063,type=float,metavar='LR',help='initial learning rate') #0.000063
 parser.add_argument('--drop','--dropout',default=0,type=float,metavar='Dropout',help='Dropout ratio')
 parser.add_argument('--schedule',type=int,nargs='+',default=[5,10,20,30,50,70],help='Decrease learning rate at these epochs')
 parser.add_argument('--gamma',type=float,default=0.5,help='LR is multiplied by gamma on schedule')
@@ -32,7 +32,7 @@ parser.add_argument('--momentum',default=0.9,type=float,metavar='M',help='moment
 parser.add_argument('--weight_decay','--wd',default=1e-4,type=float,metavar='W',help='weight decay (default: 1e-4)')
 parser.add_argument('--panelty','--pl',default=1e-4,type=float)
 parser.add_argument('--checkpoint',default='/content/checkpoints',type=str,metavar='PATH',help='path to save checkpoint(default:checkpoint)')
-parser.add_argument('--resume',default='',type=str,metavar='PATH',help='path to latest checkpoint(default:None)')
+parser.add_argument('--resume',default='/content/drive/My Drive/assess_boneage/assess_boneage/1230/Assess_BoneAge_InceptionV3_33.pth.tar',type=str,metavar='PATH',help='path to latest checkpoint(default:None)')
 parser.add_argument('--depth',type=int,default=104,help='Model depth')
 parser.add_argument('--cardinality',type=int,default=8,help='Model cardinality(group)')
 parser.add_argument('--widen_factor',type=int,default=4,help='Widen factor 4 -> 64,8 -> 128')
@@ -207,7 +207,7 @@ def save_checkpoint(state,is_best,checkpoint='checkpoint',filename='checkpoint.p
     filepath = os.path.join(checkpoint,filename)
     torch.save(state,filepath)
     if is_best:
-        shutil.copyfile(filepath,os.path.join("/content/drive/My Drive/assess_boneage/assess_boneage/1214",'model_best.pth.tar'))
+        shutil.copyfile(filepath,os.path.join("/content/drive/My Drive/assess_boneage/assess_boneage/1230",'model_best.pth.tar'))
 
 def adjust_learning_rate(optimizer,epoch):
     global state
