446a1e5012e0a5d250c06e97977ef3b1c92b2a99
xiaolebi <73866848+xiaolebi@users.noreply.github.com>
2021-01-15 09:34:37 +0000

    Update train.py

--- assess_boneage/train.py
+++ assess_boneage/train.py
@@ -5,6 +5,8 @@ import shutil
 import time
 import random
 import torch
+import logging
+import logging.handlers
 import torch.nn as nn
 import torch.nn.parallel
 import torch.backends.cudnn as cudnn
@@ -22,17 +24,17 @@ parser.add_argument('--dataset',default='BoneageAssessmentDataset')
 parser.add_argument('--workers',default=2,type=int,metavar='N',help='number of data loading workers (default:4)')
 parser.add_argument('--epochs',default=120,type=int,metavar='N',help='number of total epochs to run')
 parser.add_argument('--start_epoch',default=0,type=int,metavar='N',help='manual epoch number (useful on restarts)')
-parser.add_argument('--train_batch',default=16,type=int,metavar='N',help='train batch size')
-parser.add_argument('--test_batch',default=3,type=int,metavar='N',help='test batch size')
-parser.add_argument('--lr','--learning-rate',default=0.000063,type=float,metavar='LR',help='initial learning rate') #0.000063
+parser.add_argument('--train_batch',default=32,type=int,metavar='N',help='train batch size')
+parser.add_argument('--test_batch',default=8,type=int,metavar='N',help='test batch size')
+parser.add_argument('--lr','--learning-rate',default=0.001,type=float,metavar='LR',help='initial learning rate') #0.000063
 parser.add_argument('--drop','--dropout',default=0,type=float,metavar='Dropout',help='Dropout ratio')
-parser.add_argument('--schedule',type=int,nargs='+',default=[10,20,30,50,70,80],help='Decrease learning rate at these epochs')
+parser.add_argument('--schedule',type=int,nargs='+',default=[5,10,20,30,50,70],help='Decrease learning rate at these epochs')
 parser.add_argument('--gamma',type=float,default=0.5,help='LR is multiplied by gamma on schedule')
 parser.add_argument('--momentum',default=0.9,type=float,metavar='M',help='momentum')
 parser.add_argument('--weight_decay','--wd',default=1e-4,type=float,metavar='W',help='weight decay (default: 1e-4)')
 parser.add_argument('--panelty','--pl',default=1e-4,type=float)
 parser.add_argument('--checkpoint',default='/content/checkpoints',type=str,metavar='PATH',help='path to save checkpoint(default:checkpoint)')
-parser.add_argument('--resume',default='/content/checkpoints/resume/Assess_BoneAge_InceptionV3_57.pth.tar',type=str,metavar='PATH',help='path to latest checkpoint(default:None)')
+parser.add_argument('--resume',default='',type=str,metavar='PATH',help='path to latest checkpoint(default:None)')
 parser.add_argument('--depth',type=int,default=104,help='Model depth')
 parser.add_argument('--cardinality',type=int,default=8,help='Model cardinality(group)')
 parser.add_argument('--widen_factor',type=int,default=4,help='Widen factor 4 -> 64,8 -> 128')
@@ -53,6 +55,13 @@ torch.manual_seed(args.manualSeed)
 if use_cuda:
     torch.cuda.manual_seed_all(args.manualSeed)
 best_acc = 999
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+handler = logging.FileHandler('/content/drive/My Drive/assess_boneage/assess_boneage/0115/20210115.log')
+fmt = logging.Formatter('[%(asctime)s] - %(filename)s [Line:%(lineno)d] - [%(levelname)s] - %(message)s')
+handler.setFormatter(fmt)
+handler.setLevel(logging.INFO)
+logger.addHandler(handler)
 
 def main():
     global best_acc
@@ -98,6 +107,7 @@ def main():
     title = 'Assess_BoneAge_InceptionV3'
     if args.resume:
         print('==> Resume from checkpoint...')
+        logging.info('==> Resume from checkpoint %s'%(args.resume))
         assert os.path.isfile(args.resume)
         checkpoint = torch.load(args.resume)
         best_acc = checkpoint['best_acc']
@@ -119,7 +129,8 @@ def main():
         print('\nLR:%f train_loss:%s trian_acc:%s test_loss:%s test_acc:%s' %(state['lr'],train_loss,trian_acc,test_loss,test_acc))
         is_best = test_loss<best_acc
         best_acc = min(test_loss,best_acc)
-        if (epoch+1)%5 == 0 or test_loss<5.85:
+        logging.info('LR:%f epoch:%s train_loss:%s test_loss:%s best_loss:%s'%(state['lr'],epoch,train_loss,test_loss,best_acc))
+        if (epoch+1)%5 == 0 or test_loss<5.9:
             save_checkpoint({
                 'epoch':epoch+1,
                 'state_dict':model.state_dict(),
@@ -207,7 +218,7 @@ def save_checkpoint(state,is_best,checkpoint='checkpoint',filename='checkpoint.p
     filepath = os.path.join(checkpoint,filename)
     torch.save(state,filepath)
     if is_best:
-        shutil.copyfile(filepath,os.path.join("/content/drive/My Drive/assess_boneage/assess_boneage/0114",'model_best.pth.tar'))
+        shutil.copyfile(filepath,os.path.join("/content/drive/My Drive/assess_boneage/assess_boneage/0115",'model_best.pth.tar'))
 
 def adjust_learning_rate(optimizer,epoch):
     global state
