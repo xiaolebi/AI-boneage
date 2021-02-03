f584bc91755827e52aeecefae2448d3adb31bf4e
xiaolebi <73866848+xiaolebi@users.noreply.github.com>
2021-02-03 13:48:40 +0800

    Update train.py

--- assess_boneage/train.py
+++ assess_boneage/train.py
@@ -32,7 +32,7 @@ parser.add_argument('--test_batch',default=3,type=int,metavar='N',help='test bat
 parser.add_argument('--lr','--learning-rate',default=0.001,type=float,metavar='LR',help='initial learning rate') #0.000063
 parser.add_argument('--drop','--dropout',default=0,type=float,metavar='Dropout',help='Dropout ratio')
 parser.add_argument('--schedule',type=int,nargs='+',default=[4,8,16,32,64,100],help='Decrease learning rate at these epochs')
-parser.add_argument('--gamma',type=float,default=0.1,help='LR is multiplied by gamma on schedule')
+parser.add_argument('--gamma',type=float,default=0.5,help='LR is multiplied by gamma on schedule')
 parser.add_argument('--momentum',default=0.9,type=float,metavar='M',help='momentum')
 parser.add_argument('--weight_decay','--wd',default=1e-4,type=float,metavar='W',help='weight decay (default: 1e-4)')
 parser.add_argument('--panelty','--pl',default=1e-4,type=float)
