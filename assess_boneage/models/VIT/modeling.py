69b6986b9861b6f8c9b659891ff9ae3df5734bf5
xiaolebi <73866848+xiaolebi@users.noreply.github.com>
2021-03-04 15:35:11 +0800

    Update modeling.py

--- assess_boneage/models/VIT/modeling.py
+++ assess_boneage/models/VIT/modeling.py
@@ -18,7 +18,7 @@ from torch.nn import CrossEntropyLoss, Dropout, Softmax, Linear, Conv2d, LayerNo
 from torch.nn.modules.utils import _pair
 from scipy import ndimage
 
-import configs as configs
+import .configs as configs
 
 from modeling_resnet import ResNetV2
 from torchsummary import summary
