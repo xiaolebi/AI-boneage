4a31fc346bc5411833a987cdd936b6add2398b41
xiaolebi <73866848+xiaolebi@users.noreply.github.com>
2021-03-05 10:49:22 +0800

    Update train.py

--- assess_boneage/train.py
+++ assess_boneage/train.py
@@ -29,7 +29,7 @@ parser.add_argument('--dataset',default='BoneageAssessmentDataset')
 parser.add_argument('--workers',default=4,type=int,metavar='N',help='number of data loading workers (default:4)')
 parser.add_argument('--epochs',default=120,type=int,metavar='N',help='number of total epochs to run')
 parser.add_argument('--start_epoch',default=0,type=int,metavar='N',help='manual epoch number (useful on restarts)')
-parser.add_argument('--train_batch',default=128,type=int,metavar='N',help='train batch size')
+parser.add_argument('--train_batch',default=256,type=int,metavar='N',help='train batch size')
 parser.add_argument('--test_batch',default=32,type=int,metavar='N',help='test batch size')
 parser.add_argument('--lr','--learning-rate',default=0.1,type=float,metavar='LR',help='initial learning rate') #0.000125
 parser.add_argument('--drop','--dropout',default=0,type=float,metavar='Dropout',help='Dropout ratio')
