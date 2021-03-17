7f72d55c6f1ac5c47a1d173885c5f28f2b2a8015
xiaolebi <73866848+xiaolebi@users.noreply.github.com>
2021-03-17 16:41:58 +0800

    Update BoneAgeNet.py

--- assess_boneage/models/BoneAgeNet.py
+++ assess_boneage/models/BoneAgeNet.py
@@ -7,6 +7,7 @@ from .SE_inceptionv3 import SEInception_v3
 from .vit import ViT
 from .VIT.modeling import VisionTransformer
 from .InceptionV3 import Inception_v3
+from .InceptionV3_SE_PAM_CAM import Inception_v3_SE_PAM
 
 class BoneAge(nn.Module):
     def __init__(self,num_class = 1):
@@ -31,6 +32,25 @@ class BoneAge_InceptionV3_NO_SE(nn.Module):
     def __init__(self,num_class = 1):
         super(BoneAge_InceptionV3_NO_SE,self).__init__()
         pretrain_net = Inception_v3()
+        self.base_net = pretrain_net
+        self.gender_dense = nn.Linear(1,32)
+        self.fc1 = nn.Linear(2048*4 + 1*32,1000)
+        self.last_layer = nn.Linear(1000,num_class)
+
+    def forward(self,x,gender_input):
+        x = self.base_net(x)
+        gender_dense = self.gender_dense(gender_input)
+        x = torch.cat((x,gender_dense),dim=-1)
+        x = self.fc1(x)
+        x = F.elu(x)
+        x = F.dropout(x,p=0.5,training=self.training)
+        last_output = self.last_layer(x)
+        return last_output
+    
+class BoneAge_InceptionV3_SE_PAM(nn.Module):
+    def __init__(self,num_class = 1):
+        super(BoneAge_InceptionV3_SE_PAM,self).__init__()
+        pretrain_net = Inception_v3_SE_PAM()
         self.base_net = pretrain_net
         self.gender_dense = nn.Linear(1,32)
         self.fc1 = nn.Linear(2048*4 + 1*32,1000)
