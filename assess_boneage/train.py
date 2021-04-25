d8cf84d234df23c60e7bcdd7f58e31058e49b952
xiaolebi <73866848+xiaolebi@users.noreply.github.com>
2021-04-25 14:54:28 +0800

    Update train.py

--- assess_boneage/train.py
+++ assess_boneage/train.py
@@ -31,7 +31,7 @@ parser.add_argument('--epochs',default=120,type=int,metavar='N',help='number of 
 parser.add_argument('--start_epoch',default=0,type=int,metavar='N',help='manual epoch number (useful on restarts)')
 parser.add_argument('--train_batch',default=16,type=int,metavar='N',help='train batch size')
 parser.add_argument('--test_batch',default=4,type=int,metavar='N',help='test batch size')
-parser.add_argument('--lr','--learning-rate',default=0.000125,type=float,metavar='LR',help='initial learning rate') #0.000125
+parser.add_argument('--lr','--learning-rate',default=0.001,type=float,metavar='LR',help='initial learning rate') #0.000125
 parser.add_argument('--drop','--dropout',default=0,type=float,metavar='Dropout',help='Dropout ratio')
 parser.add_argument('--schedule',type=int,nargs='+',default=[5,10,20,30,50],help='Decrease learning rate at these epochs')
 parser.add_argument('--gamma',type=float,default=0.5,help='LR is multiplied by gamma on schedule')
@@ -39,7 +39,7 @@ parser.add_argument('--momentum',default=0.9,type=float,metavar='M',help='moment
 parser.add_argument('--weight_decay','--wd',default=1e-4,type=float,metavar='W',help='weight decay (default: 1e-4)')
 parser.add_argument('--panelty','--pl',default=1e-4,type=float)
 parser.add_argument('--checkpoint',default='/content/checkpoints',type=str,metavar='PATH',help='path to save checkpoint(default:checkpoint)')
-parser.add_argument('--resume',default='/content/checkpoints/resume/model_best_inception_SE_PAM_CAM.pth.tar',type=str,metavar='PATH',help='path to latest checkpoint(default:None)')#/content/checkpoints/resume/Assess_BoneAge_InceptionV3_4.pth.tar
+parser.add_argument('--resume',default='',type=str,metavar='PATH',help='path to latest checkpoint(default:None)')#/content/checkpoints/resume/Assess_BoneAge_InceptionV3_4.pth.tar
 parser.add_argument('--depth',type=int,default=104,help='Model depth')
 parser.add_argument('--cardinality',type=int,default=8,help='Model cardinality(group)')
 parser.add_argument('--widen_factor',type=int,default=4,help='Widen factor 4 -> 64,8 -> 128')
@@ -62,7 +62,7 @@ if use_cuda:
 best_acc = 999
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
-handler = logging.FileHandler('/content/drive/My Drive/assess_boneage/assess_boneage/0422/20210422_inception_SE_PAM_CAM.log')
+handler = logging.FileHandler('/content/drive/My Drive/assess_boneage/assess_boneage/0425/20210425_original_dataset_inception_SE_PAM_CAM.log')
 fmt = logging.Formatter('[%(asctime)s] - %(filename)s [Line:%(lineno)d] - [%(levelname)s] - %(message)s')
 handler.setFormatter(fmt)
 handler.setLevel(logging.INFO)
@@ -157,7 +157,7 @@ def main():
         is_best = test_loss<best_acc
         best_acc = min(test_loss,best_acc)
         logging.info('LR:%f epoch:%s train_loss:%s test_loss:%s best_loss:%s'%(state['lr'],epoch,train_loss,test_loss,best_acc))
-        if (epoch+1)%5 == 0 or test_loss<6.1:
+        if (epoch+1)%5 == 0 or test_loss<6.8:
             save_checkpoint({
                 'epoch':epoch+1,
                 'state_dict':model.state_dict(),
@@ -249,7 +249,7 @@ def save_checkpoint(state,is_best,checkpoint='checkpoint',filename='checkpoint.p
     filepath = os.path.join(checkpoint,filename)
     torch.save(state,filepath)
     if is_best:
-        shutil.copyfile(filepath,os.path.join("/content/drive/My Drive/assess_boneage/assess_boneage/0422",'model_best_inception_SE_PAM_CAM.pth.tar'))
+        shutil.copyfile(filepath,os.path.join("/content/drive/My Drive/assess_boneage/assess_boneage/0425",'model_best_inception_SE_PAM_CAM.pth.tar'))
 
 def adjust_learning_rate(optimizer,epoch):
     global state
